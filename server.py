865ce305a8c94dce921a917fec7dae0d46626b81
darthvader <darthvader@pop-os.localdomain>
2019-11-30 23:40:47 +0000

    Added serv_log function and started work on the registration

--- server.py
+++ server.py
@@ -14,8 +14,40 @@ class Client:
     def update_timestamp(self):
         self.timestamp = time.time()
 
+    def reg_nick(nick):
+        self.nick = nick
+
+    def reg_user(self, username, hostname, servername, realname):
+        self.username = username
+        self.hostname = hostname
+        self.servername = servename
+        self.realname = realname
+        
+
 HOST = '127.0.0.1'
 PORT = 6667
+
+def serv_log(text):
+    print("[Server] {}".format(text))
+
+def reg_msg_parser(msg):
+    serv_log(msg)
+
+    if len(msg) == 0 :
+        return
+
+    if msg.find("CAP") != -1:
+        return
+
+    mp = MessageParser()
+
+    try:
+        prefix, command, params = mp.parseMessage(msg)
+    except ValueError as e:
+        print(e)
+
+    serv_log(command)
+    serv_log(params)
 
 
 def accept_connection(server_sock):
@@ -24,22 +56,23 @@ def accept_connection(server_sock):
     mask = selectors.EVENT_READ | selectors.EVENT_WRITE
     sock_selector.register(conn, mask, data=Client())
 
-    print('{} connected!'.format(addr))
+    serv_log('{} connected!'.format(addr))
 
     mp = MessageParser()
-    print(conn.recv(512).decode())
 
     messages = conn.recv(512).decode().split('\n')
 
+    serv_log('{} connected!'.format(addr))
     for m in messages:
         if m != '':
             prefix, command, params = mp.parseMessage(m)
-            print("prefix:", prefix)
-            print("command:", command)
-            print("params:", params)
+            serv_log("prefix:" + prefix)
+            serv_log("command:" + command)
+            serv_log("params:" + params)
 
 
 
+    
 def service_connection(key, mask):
     conn = key.fileobj
     data = key.data
@@ -63,7 +96,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         # Get a list of all sockets which are ready to be written to, read from, or both
         events = sock_selector.select(timeout=None)
-
         for key, mask in events:
             if key.data == None:
                 accept_connection(key.fileobj)
